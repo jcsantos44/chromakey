0afa35b4e916e397c6e6935a7425948a7c216e9e
amil95 <amilzito@gmail.com>
2019-10-22 19:09:34 -0300

    Update wowniga.py

--- wowniga.py
+++ wowniga.py
@@ -5,14 +5,16 @@ from matplotlib import pyplot as plt
 bg = cv2.imread('sesto.jpg').astype(np.float32)/255
 bgb,bgg,bgr = cv2.split(bg)
 
-for i in range(3, 8):
+for i in range(4, 8):
     imgName = str(i) + '.bmp'
     img = cv2.imread(imgName).astype(np.float32)/255
     img2 = cv2.imread(imgName, 0).astype (np.float32)/255
 
     b,g,r = cv2.split(img)
-    img2 = g - b - r
+    img2 = g
     img2[img2 < 0] = 0
+    
+    img2[img2 > 1] = 1
     
     """cv2.imshow(imgName, np.asarray(img2))
     cv2.waitKey()
@@ -25,8 +27,9 @@ for i in range(3, 8):
             if(gaux > 0.3 and gaux < 0.7):
                 raux = (1 - gaux) * r[m][n] + gaux * bgr[m][n]
                 baux = (1 - gaux) * b[m][n] + gaux * bgb[m][n]
-                gaux = (1 - gaux) * g[m][n] + gaux * bgg[m][n]
+                gaux = ((1 - gaux) * g[m][n] + gaux * bgg[m][n])/2
                 img[m][n] = [baux, gaux, raux]
+            #print(img[m][n])
             if(gaux >= 0.7):
                 img[m][n] = bg[m][n]
             
